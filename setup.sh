33a4ea0f6518f1da4b4dc0c1e383382f9737b382
Connor Abra <connor@abra.id.au>
2024-09-10 18:11:57 +1000

    Update setup script to auto-download code and add run script

--- setup.sh
+++ setup.sh
@@ -4,16 +4,20 @@ sudo apt update
 sudo apt full-upgrade
 
 # Install python dev tools and pyaudio dependencies to allow building pip packages in the venv
-sudo apt install python3-dev portaudio19-dev
+sudo apt install git python3-dev portaudio19-dev
+
+# Download guestbook code and change to the code directory
+git clone https://github.com/abrakidabra/PhoneGuestbook.git ./PhoneGuestbook
+cd PhoneGuestbook
 
 # Create virtualenv and source it
 python -m venv .venv
 source .venv/bin/activate
 
-# Install required pip packages
+# Install required pip packages into the venv
 pip install gpiozero lgpio pyaudio
 
 # Patch lgpio to work on the Pi 5
 sed -i 's/chip = 4 if (self._get_revision() & 0xff0) >> 4 == 0x17 else 0/chip = 0 if (self._get_revision() & 0xff0) >> 4 == 0x17 else 0/g' .venv/lib/python3.11/site-packages/gpiozero/pins/lgpio.py
 
-echo "Setup completed!"
+echo "Setup completed! Run PhoneGuestbook/run.sh to start the program"
