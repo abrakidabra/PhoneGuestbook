182bf3b8c2f5d96d9a6a78aa2df3ee26172abcc9
Connor Abra <connor@abra.id.au>
2024-09-11 17:38:35 +1000

    Update guestbook to use external config file  and add comments

--- guestbook.py
+++ guestbook.py
@@ -5,22 +5,35 @@ import pyaudio
 import wave
 import datetime
 import atexit
+import configparser
 
+# Read config data from the config file
+config = configparser.ConfigParser()
+config.read("config.cfg")
+greeting_path = config["Files"]["greeting_path"]
+output_folder = config["Files"]["output_folder"]
+handset_signal_pin = config["GPIO"]["handset_signal_pin"]
+
+# Configure settings for the recording device
 chunk = 1024
 sample_format = pyaudio.paInt16
 channels = 1
 fs = 44100
 p = pyaudio.PyAudio()
 
-button = Button(5, bounce_time=0.25)
+# Initialise the GPIO button
+handset_switch = Button(handset_signal_pin, bounce_time=0.25)
 
+# Initialise greeting playback
 mixer.init()
-
-greeting = mixer.Sound("greeting.wav")
+greeting = mixer.Sound(greeting_path)
 
 def recordAudio():
+    # Generate filename for current recording based on configured folder path and current time
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    filename = f"recording-{str(current_time)}.wav"
+    filename = f"{output_folder}recording-{str(current_time)}.wav"
+    
+    # Play greeting audio and begin recording
     greeting.play()
     print("Recording...")
 
@@ -29,9 +42,10 @@ def recordAudio():
                     rate = fs,
                     frames_per_buffer =  chunk,
                     input = True)
-
+    
+    # Keep recording while handset is lifted and store audio in temp buffer
     frames = []
-    while button.is_pressed:
+    while handset_switch.is_pressed:
         data = stream.read(chunk)
         frames.append(data)
 
@@ -40,6 +54,7 @@ def recordAudio():
     print("Recording ended... Writing to " + filename + "...")
     greeting.stop()
 
+    # Write audio from buffer to output file
     with wave.open(filename, 'wb') as wf:
         wf.setnchannels(channels)
         wf.setsampwidth(p.get_sample_size(sample_format))
@@ -48,7 +63,8 @@ def recordAudio():
         wf.close()
     print("Write complete")
 
-button.when_pressed = recordAudio
+# Set up event handler for the
+handset_switch.when_pressed = recordAudio
 
 def exit_handler():
     p.terminate()
